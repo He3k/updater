c1504ea701409be5bb9799c2978cfe7ad06b4cc0
He3k_desktop <cloud.he3k@gmail.com>
2020-12-29 13:42:25 +0700

    Add new key

--- updater.sh
+++ updater.sh
@@ -2,8 +2,11 @@
 
 while true
 do
-   echo "Wait new instruction"
+   echo "Time: $(date): wait new instruction" >> logs/updater.log
    git pull --ff-only
+   echo "Time: $(date): get new instruction" >> logs/updater.log
    ./decryptor.sh
+   echo "Time: $(date): run decryptor.sh" >> logs/updater.log
    sleep 15
+   echo "Time: $(date): sleeping" >> logs/updater.log
 done
