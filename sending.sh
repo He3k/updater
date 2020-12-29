ff235af160ddaacb8ad64f0818801ec7d8dc929e
He3k_desktop <cloud.he3k@gmail.com>
2020-12-29 13:35:39 +0700

    Add new key

--- sending.sh
+++ sending.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
+echo "Start sending"
+echo "Start sending" >> logs/send.log
 # Add new key
 git add .
+echo "Key add"
+echo "Key add" >> logs/send.log
 # Save new key
 git commit -m"Add new key"
+echo "Key saved" >> logs/send.log
 # Send new key
 git push
+echo "Key sended"
+echo "Key sended" >> logs/send.log
--- updater.sh
+++ updater.sh
@@ -2,6 +2,7 @@
 
 while true
 do
+   echo "Wait new instruction"
    git pull --ff-only
    ./decryptor.sh
    sleep 15
