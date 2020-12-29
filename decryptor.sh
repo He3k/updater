aee1cd7d56cc0fae30b40d1db749dfafe98b48bb
He3k_desktop <cloud.he3k@gmail.com>
2020-12-29 14:32:57 +0700

    Update

--- decryptor.sh
+++ decryptor.sh
@@ -2,11 +2,10 @@
 
 # aircrack-ng handshake/WPA.cap -w library/wordlist.txt -l key/key.txt
 # check handshake
-if [ -e handshake/WPA.cap ]
-then
-echo "Time: $(date): handshake exist"
-echo "Time: $(date): handshake exist" >> logs/decryptor.log
+
 # decrypting
+echo "Time: $(date): start decrypting"
+echo "Time: $(date): start decrypting" >> logs/decryptor.log
 aircrack-ng handshake/WPA.cap -w library/wordlist.txt -l key/key.txt
 echo "Time: $(date): end decrypting"
 echo "Time: $(date): end decrypting" >> logs/decryptor.log
@@ -18,9 +17,4 @@ echo "Time: $(date): remove old handshake" >> logs/decryptor.log
 ./sending.sh
 echo "Time: $(date): run sending.sh"
 echo "Time: $(date): run sending.sh" >> logs/decryptor.log
-else
-# bad result check
-echo "Time: $(date): handshake doesn't exist"
-echo "Time: $(date): handshake doesn't exist" >> logs/decryptor.log
-fi
-exit
+
--- updater.sh
+++ updater.sh
@@ -5,11 +5,14 @@ do
    echo "Time: $(date): wait new instruction"
    echo "Time: $(date): wait new instruction" >> logs/updater.log
    git pull --ff-only
+   if [ -e handshake/WPA.cap ]
+   then
    echo "Time: $(date): get new instruction"
    echo "Time: $(date): get new instruction" >> logs/updater.log
    ./decryptor.sh
    echo "Time: $(date): run decryptor.sh"
    echo "Time: $(date): run decryptor.sh" >> logs/updater.log
+   fi
    sleep 15
    echo "Time: $(date): sleeping"
    echo "Time: $(date): sleeping" >> logs/updater.log
